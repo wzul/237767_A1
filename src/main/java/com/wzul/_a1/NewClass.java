b548d462ae35ed84de9ddd2642f5003deeed4974
Wan Zulkarnain <wan@wanzul-hosting.com>
2017-10-22 15:09:24 +0800

    123 testing

---
+++ src/main/java/com/wzul/_a1/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wzul._a1;
+
+/**
+ *
+ * @author wanzu
+ */
+public class NewClass {
+    
+}
