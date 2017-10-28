7b6db5d71af00d328e2ce9aaecc36090c69b758e
Wan Zulkarnain <wan@wanzul-hosting.com>
2017-10-28 21:55:43 +0800

    Mac OS Compatible

--- src/main/java/Folder/Path.java
+++ src/main/java/Folder/Path.java
@@ -21,7 +21,8 @@ public class Path {
      * @return File
      */
     public static String getPath(String folderName) {
-        return Paths.get("").toAbsolutePath().toString() + "\\" + folderName;
+        return System.getProperty("user.dir") + File.separator + folderName;
+        //return Paths.get("").toAbsolutePath().toString() + "\\" + folderName;
     }
 
 }
